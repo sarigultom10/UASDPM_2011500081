020b65892e95f47ed3896ee5479369a9fc11fe38
sarigultom10 <106232822+sarigultom10@users.noreply.github.com>
2022-06-21 23:19:56 +0700

    Initial commit

---
+++ app/src/main/java/com/example/uasdpm_2011500081/DataDosen.kt
@@ -0,0 +1,11 @@
+package com.example.uasdpm_2011500081
+
+class DataDosen(
+    var Nidn: String,
+    var NmDosen: String,
+    var jabatan: String,
+    var GolPangkat: String,
+    var pendidikan: String,
+    var keahlian: String,
+    var ProgramStudi:String
+)
---
+++ app/src/main/java/com/example/uasdpm_2011500081/layout_data_dosen.kt
@@ -0,0 +1,11 @@
+package com.example.uasdpm_2011500081
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class layout_data_dosen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_layout_data_dosen)
+    }
+}
